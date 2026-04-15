09e66e72d6317366f631166f6ebf4bb097c904ed
jtang47-code <tang.jieworking@outlook.com>
2026-04-15 09:32:12 -0400

    Change apiBaseUrl in app-config.js
    Update apiBaseUrl for Cloudflare Tunnel.

--- app-config.js
+++ app-config.js
@@ -1,7 +1,7 @@
 window.APP_CONFIG = window.APP_CONFIG || {
   // Public backend URL exposed through Cloudflare Tunnel. Update everytime restart Cloudflare
   // Example: "https://api.yourdomain.com"
-  apiBaseUrl: "https://dover-engineering-considered-procurement.trycloudflare.com",
+  apiBaseUrl: "https://ranges-stopping-dame-sleeve.trycloudflare.com",
 
 
 
