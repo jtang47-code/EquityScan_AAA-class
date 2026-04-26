fdcba237157e681a3121d6e77b7ac492ff27bca7
jtang47-code <tang.jieworking@outlook.com>
2026-04-26 17:11:20 -0400

    Update app-config.js

--- app-config.js
+++ app-config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = window.APP_CONFIG || {
   // Public backend URL exposed through Cloudflare Tunnel. Update everytime restart Cloudflare
   // Example: "https://api.yourdomain.com"
-  apiBaseUrl: ""
+  apiBaseUrl: "https://meet-prior-gibraltar-creature.trycloudflare.com "
 };
